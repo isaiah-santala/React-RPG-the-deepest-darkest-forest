a0361306ead44a0d555e10c74fc04933b6fa8e7a
Isaiah Santala <Isantala@yahoo.com>
2019-10-30 10:20:04 -0700

    updated app to class component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 import HomeView from './components/HomeView/HomeView'
 import { generatePlayer, generateNewEnemy } from './logic/generators/generators'
+import CombatView from './components/CombatView/CombatView';
 
-const App = () => {
-  const player = generatePlayer('Bob')
-  const enemy = generateNewEnemy(2)
-  console.log(player, enemy)
-  return (
-    <div>
-      <HomeView/>
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      player: generatePlayer('Bob', 'I am Bob'),
+      enemy: generateNewEnemy(0),
+      view: 'COMBAT'
+    }
+  }
+
+  generatePlayer(name, saying) {
+    this.setState({
+      player: generatePlayer(name, saying)
+    })
+  }
+
+  generateNewEnemy(playerLvl) {
+    this.setState({
+      enemy: generateNewEnemy(playerLvl)
+    })
+  }
+
+  updateView(view) {
+    this.setState({ view })
+  }
+
+  render() {
+    const  { view, player, enemy } = this.state
+
+    return (
+      <div>
+        {view === 'LOGIN' && 
+          <Login
+        />}
+        {view === 'HOME' && 
+          <HomeView 
+            player={player}
+        />}
+        {view === 'COMBAT' && 
+          <CombatView
+            player={player}
+            enemy={enemy}
+        />}
+      </div>
+    )
+  }
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// import App from "./App";
+import App from "./App";
 
-// ReactDOM.render(<App />, document.getElementById('App'))
-
-import CombatView from './components/CombatView/CombatView'
-
-ReactDOM.render(<CombatView />, document.getElementById('App'))
+ReactDOM.render(<App />, document.getElementById('App'))
